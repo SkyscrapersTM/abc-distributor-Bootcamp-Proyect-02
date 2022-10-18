fa17bffbf189132e8051a5fb9b0236a63bb3a499
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2022-10-17 19:14:25 -0500

    add productModel

--- abc_distributor/apps/warehouse/products/admin.py
+++ abc_distributor/apps/warehouse/products/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from apps.warehouse.products.models import UnitMeasureCategory
 from apps.warehouse.products.models import UnitMeasure
+from apps.warehouse.products.models import ProductCategory
+from apps.warehouse.products.models import Product
 
 admin.site.register(UnitMeasureCategory)
 admin.site.register(UnitMeasure)
+admin.site.register(ProductCategory)
+admin.site.register(Product)
